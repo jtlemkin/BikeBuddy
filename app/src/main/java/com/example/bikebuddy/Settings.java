187d8fad2757f2f796e3f771b35941c6942e276b
jtlemkin <jtlemkin@gmail.com>
2020-04-29 16:18:03 -0700

    Implemented navigating back to main

--- app/src/main/java/com/example/bikebuddy/Settings.java
+++ app/src/main/java/com/example/bikebuddy/Settings.java
@@ -1,13 +1,19 @@
 package com.example.bikebuddy;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -15,6 +21,7 @@ import android.view.ViewGroup;
  */
 public class Settings extends Fragment {
 
+    private View activity;
 
     public Settings() {
         // Required empty public constructor
@@ -26,6 +33,23 @@ public class Settings extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        activity = getView();
+
+        enableNavigationToMain();
+    }
+
+    private void enableNavigationToMain() {
+        Button backButton = activity.findViewById(R.id.back_button);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Navigation.findNavController(view).popBackStack();
+            }
+        });
     }
 
 }
