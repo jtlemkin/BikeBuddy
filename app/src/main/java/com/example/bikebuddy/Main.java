f3f4b4e299d0e82ec312829356480a2d2ec18a0e
jtlemkin <jtlemkin@gmail.com>
2020-04-29 15:48:29 -0700

    Implemented navigation to settings from Main Fragment

--- app/src/main/java/com/example/bikebuddy/Main.java
+++ app/src/main/java/com/example/bikebuddy/Main.java
@@ -13,11 +13,13 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -97,6 +99,17 @@ public class Main extends Fragment implements OnMapReadyCallback {
         setupArmedText();
         mBatteryText = activity.findViewById(R.id.batteryText);
         updateBatteryText();
+        enableNavigationToSettings();
+    }
+
+    private void enableNavigationToSettings() {
+        Button settingsButton = activity.findViewById(R.id.settings_button);
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_main_to_settings);
+            }
+        });
     }
 
     private void updateUIOnDeviceConnect() {
--- app/src/main/java/com/example/bikebuddy/MainActivity.java
+++ app/src/main/java/com/example/bikebuddy/MainActivity.java
@@ -1,39 +1,8 @@
 package com.example.bikebuddy;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothManager;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.CompoundButton;
-import android.widget.TextView;
-import android.widget.ToggleButton;
-
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
 import java.util.Objects;
-
-/*
-   TASKS:
-   1. Implement bluetooth capabilities
-    e. Bluetooth connections in background
-    f. Start bluetooth service with jobscheduler
-   2. Change map annotation to be white and contain bike emoji
- */
-
 
 public class MainActivity extends AppCompatActivity {
 
